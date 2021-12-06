591af5f825860d6b765df832dfbbfc7c58a90e48
RonildoSilvaOSF <83358803+RonildoSilvaOSF@users.noreply.github.com>
2021-12-06 15:36:20 -0300

    Guardas e rotas de login

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../components/login/login.component';
 import { HomeModule } from '../components/home/home.module';
+import { SocialNetGuard } from '../guards/social-net.guard';
 
 const routes: Routes = [
   //Lazy load module 
   {
     path: '', 
     loadChildren: () => import('../components/home/home.module')
-    .then((m:{HomeModule:HomeModule}) => m.HomeModule)
+    .then((m:{HomeModule:HomeModule}) => m.HomeModule),
+    canActivate: [SocialNetGuard]
   },
   {
     path: 'login', 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { HomeModule } from './components/home/home/home.module';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 
